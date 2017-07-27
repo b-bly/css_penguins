bc304e5f22d0c42577b04aabae8b3311b4a51661
Brendt Bly <brendtbly@Brendts-MacBook-Air.local>
2017-07-27 17:13:57 -0500

    Added random appearance of yeti

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(document).ready( function() {
     console.log('jqeury sourced');
     //This code will run after your page loads
+    var penguinClass = '.penguin';
+    penguinClass += Math.floor(Math.random()*8);
+    var randPenguin = $(penguinClass);
+
+    var yeti = $('.yeti');
+
+    //insert yeti before randPenguin
+    yeti.insertBefore(randPenguin);
+    //remove yeti
+    yeti.remove;
 
     $(".yeti").mousedown(function() {
         alert("Yaaaarrrr!");
