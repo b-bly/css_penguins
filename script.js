016a3a8c059ef0113d5fdb9535e3a01a67f8c889
Brendt Bly <brendtbly@Brendts-MacBook-Air.local>
2017-07-27 10:47:47 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready( function() {
+    console.log('jqeury sourced');
+    //This code will run after your page loads
+
+    $(".yeti").mousedown(function() {
+        alert("Yaaaarrrr!");
+    });
+
+});
